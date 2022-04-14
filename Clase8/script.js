6bda19b92bc7b0aa78a470227ac4dacdbe33268f
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2022-04-14 10:26:17 -0300

    codigo en clase 8

---
+++ Clase8/script.js
@@ -0,0 +1,13 @@
+let password="123Victroria"
+
+let encriptada="(*&DKNSKH(*Djhdfkhs"//base de datos
+
+
+//se busca el password encriptado // se hacen las validaciones
+
+/*
+un token = un string encriptado que autoriza a la parsona por un tiempo
+determinado a acceder al sitio o sectores
+*/
+
+
